cc56cee3ac91cfdb7e1b8dee1d03b46a9b71e0e2
abhishek-2k23 <kumar.abhishek2k23@gmail.com>
2026-06-28 05:48:59 +0530

    initial

---
+++ app.js
@@ -0,0 +1,34 @@
+var tasks = [];
+
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var taskText = input.value;
+
+    if (taskText == "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.push(taskText);
+    renderTasks();
+
+    // bug: input not cleared after adding
+    updateCount();
+}
+
+function renderTasks() {
+    var list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    for (var i = 0; i <= tasks.length; i++) {
+        if (tasks[i] == undefined) continue;
+        var li = document.createElement("li");
+        li.innerHTML = tasks[i];
+        list.appendChild(li);
+    }
+}
+
+function updateCount() {
+    // bug: count is off by one, shows one less than actual
+    document.getElementById("count").innerText = tasks.length - 1;
+}
